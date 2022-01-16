03b5131cae84c22fbe4bc6a2e915a42259978c94
Allen Kosnik <allen.kosnik@gmail.com>
2022-01-16 08:38:35 -0600

    changed structure

--- algorithms/leetcode/isBadVersion.js
+++ algorithms/leetcode/isBadVersion.js
@@ -1,30 +1,26 @@
 const bad = 5;
 
-const isBadVersion = (num) {
+// in the example, this function is an api call
+const isBadVersion = (num) => {
   if (num >= bad) return true;
-}
-
-const solution = (isBadVersion) => {
-  /**
-   * @param {integer} n Total versions
-   * @return {integer} The first bad version
-   */
-  return (n) => {
-    let first = 1;
-    let last = n;
-    let mid = first + Math.floor((last-first) / 2);
-    let firstBad = n;
-
-    while (first <= last) {
-      mid = first + Math.floor((last-first) / 2);
-      if (isBadVersion(mid)) {
-        firstBad = mid;
-        last = mid - 1;
-      } else {
-        first = mid + 1;
-      }
-    }
-    return firstBad;
-  };
 };
 
+const findFirstBad = (n) => {
+  let first = 1;
+  let last = n;
+  let mid = first + Math.floor((last - first) / 2);
+  let firstBad = n;
+
+  while (first <= last) {
+    mid = first + Math.floor((last - first) / 2);
+    if (isBadVersion(mid)) {
+      firstBad = mid;
+      last = mid - 1;
+    } else {
+      first = mid + 1;
+    }
+  }
+  return firstBad;
+};
+
+console.log(findFirstBad(10));
