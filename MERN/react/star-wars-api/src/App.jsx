bc9bd63fed00eb50008f5dcdd8fb03ea273b65f3
Allen Kosnik <allen.kosnik@gmail.com>
2022-01-19 13:06:34 -0600

    added routing parameters

--- MERN/react/star-wars-api/src/App.jsx
+++ MERN/react/star-wars-api/src/App.jsx
@@ -1,11 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Link, Switch, Route} from 'react-router-dom';
+import Profile from './components/Profile';
+import Group from './components/Group';
+// Browser Router enables routing in enclosed elements
+// Link tag replaces anchor tags. Link does not reload page when anchor would
+// Switch defines our routes and attaches components to them
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter> 
+      <div className="App">
+        <h1>Welcome!</h1>    
+        <span>
+          <Link to="/">Home</Link>
+          <span> || </span>
+          <Link to="/profile">Profile</Link>
+          <span> || </span>
+          <Link to="/groups">Groups</Link>
+        </span>
 
-    </div>
+        <Switch>
+          <Route exact path="/">
+            <h2>Welcome to my page</h2>
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile></Profile>
+          </Route>
+          <Route exact path="/profile/:id/:color/:size">
+            <Profile></Profile>
+          </Route>
+          <Route exact path="/groups">
+            <Group></Group>
+          </Route>
+        </Switch>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ MERN/react/star-wars-api/src/components/Group.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Group() {
+  return (
+  <div>
+    <h1>MY GROUP PAGE</h1>
+  </div>
+  );
+}
