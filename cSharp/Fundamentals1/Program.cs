3f91eb8981356b693e91f917a7281af44fa102da
Allen Kosnik <allen.kosnik@gmail.com>
2022-02-07 12:41:51 -0600

    cSharp fundaments1 init commit

---
+++ cSharp/Fundamentals1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Fundamentals1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //   for (int i = 0; i <= 255; i++)
+      //   {
+      //     Console.WriteLine(i);
+      //   }
+
+      //   for (int j = 0; j < 100; j++)
+      //   {
+      //     if (j % 3 == 0 && j % 5 == 0)
+      //     {
+      //       string message = $"{j}, FizzBuzz";
+      //       Console.WriteLine(message);
+      //     }
+      //     else if (j % 3 == 0)
+      //     {
+      //       string message = $"{j}, Fizz";
+      //       Console.WriteLine(message);
+      //     }
+      //     else if (j % 5 == 0)
+      //     {
+      //       string message = $"{j}, Buzz";
+      //       Console.WriteLine(message);
+      //     }
+      //   }
+      // 
+      for
+
+    }
+  }
+}
