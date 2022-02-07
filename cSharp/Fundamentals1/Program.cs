d515c4d918168eae72ba106287d44c0354f2f88a
Allen Kosnik <allen.kosnik@gmail.com>
2022-02-07 12:43:43 -0600

    added comments

--- cSharp/Fundamentals1/Program.cs
+++ cSharp/Fundamentals1/Program.cs
@@ -6,31 +6,32 @@ namespace Fundamentals1
   {
     static void Main(string[] args)
     {
-      //   for (int i = 0; i <= 255; i++)
+      // Print numbers 1-255
+      //   for (int i = 1; i <= 255; i++)
       //   {
       //     Console.WriteLine(i);
       //   }
 
-      //   for (int j = 0; j < 100; j++)
-      //   {
-      //     if (j % 3 == 0 && j % 5 == 0)
-      //     {
-      //       string message = $"{j}, FizzBuzz";
-      //       Console.WriteLine(message);
-      //     }
-      //     else if (j % 3 == 0)
-      //     {
-      //       string message = $"{j}, Fizz";
-      //       Console.WriteLine(message);
-      //     }
-      //     else if (j % 5 == 0)
-      //     {
-      //       string message = $"{j}, Buzz";
-      //       Console.WriteLine(message);
-      //     }
-      //   }
-      // 
-      for
+      // FizzBuzz
+      for (int j = 0; j < 100; j++)
+      {
+        if (j % 3 == 0 && j % 5 == 0)
+        {
+          string message = $"{j}, FizzBuzz";
+          Console.WriteLine(message);
+        }
+        else if (j % 3 == 0)
+        {
+          string message = $"{j}, Fizz";
+          Console.WriteLine(message);
+        }
+        else if (j % 5 == 0)
+        {
+          string message = $"{j}, Buzz";
+          Console.WriteLine(message);
+        }
+      }
+
 
     }
   }
