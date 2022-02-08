0da706a086477c7efbd5b05c1356e7834c0f9545
Allen Kosnik <allen.kosnik@gmail.com>
2022-02-07 18:55:23 -0600

    added display function

--- cSharp/Fundamentals/MultiplicationTable/Program.cs
+++ cSharp/Fundamentals/MultiplicationTable/Program.cs
@@ -6,7 +6,51 @@ namespace MultiplicationTable
   {
     static void Main(string[] args)
     {
-      int[,] array2D = new int[10, 10]
+      int[,] multiplication = MultiplicationTable();
+      Print2DArray(multiplication);
+    }
+
+    public static int[,] MultiplicationTable()
+    {
+      int[,] array2D = new int[10, 10];
+      for (int i = 0; i < 10; i++)
+      {
+        for (int j = 0; j < 10; j++)
+        {
+          array2D[i, j] = (i + 1) * (j + 1);
         }
+      }
+
+      return array2D;
+    }
+
+    public static void Print2DArray(int[,] array2D)
+    {
+      Console.WriteLine(array2D.GetLength(0));
+      Console.WriteLine(array2D.GetLength(1));
+      Console.WriteLine();
+
+      for (int i = 0; i < array2D.GetLength(0); i++)
+      {
+        string message = "[ ";
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+          if (array2D[i, j] < 10)
+          {
+            message += array2D[i, j] + ",  ";
+          }
+          else if (array2D[i, j] < 100)
+          {
+            message += array2D[i, j] + ", ";
+          }
+          else
+          {
+            message += array2D[i, j] + ",";
+          }
+        }
+        message += "]";
+        Console.WriteLine(message);
+      }
+    }
   }
 }
