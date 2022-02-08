2282fcf6c871e209218ff821af992e4ea48a088c
Allen Kosnik <allen.kosnik@gmail.com>
2022-02-07 19:26:34 -0600

    Boxing init commit

---
+++ cSharp/Fundamentals/Boxing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boxing
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<object> objects = new List<object>();
+      objects.Add(7);
+      objects.Add(28);
+      objects.Add(-1);
+      objects.Add(true);
+      objects.Add("chair");
+
+      int sum = 0;
+      foreach (var item in objects)
+      {
+        Console.WriteLine("item " + item);
+        if (item is int)
+        {
+          int val = (int)item;
+          sum += val;
+        }
+      }
+      Console.WriteLine("sum " + sum);
+    }
+  }
+}
