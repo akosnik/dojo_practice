e4353989af3e6aa8211be71f9078c5092dbfa2c6
Allen Kosnik <allen.kosnik@gmail.com>
2022-02-13 16:14:39 -0600

    init commit dojosurveywithmodel

---
+++ cSharp/ASPNETCore/DojoSurveyWithModel/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DojoSurveyWithModel.Models;
+
+
+namespace DojoSurveyWithModel.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    public static Survey survey = new Survey();
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost("survey")]
+    public IActionResult Submission(Survey yourSurvey)
+    {
+      survey = yourSurvey;
+      return RedirectToAction("result");
+    }
+
+    [HttpGet("result")]
+    public IActionResult Result()
+    {
+      Console.WriteLine("SURVEY NAME ~~~~~~~ " + survey.Name);
+      return View(model: survey);
+    }
+  }
+}
