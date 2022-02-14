dcfee3e7623c1d6ab74b4bc561d43b4b67810384
Allen Kosnik <allen.kosnik@gmail.com>
2022-02-13 18:37:45 -0600

    added form validations

---
+++ cSharp/ASPNETCore/FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FormSubmission.Models;
+
+namespace FormSubmission.Controllers
+{
+  public class HomeController : Controller
+  {
+    public static User saveUser = new User();
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost("Create")]
+    public IActionResult Create(User myUser)
+    {
+      if (ModelState.IsValid)
+      {
+        saveUser = myUser;
+        return RedirectToAction("result");
+      }
+      else
+      {
+        return View("Index");
+      }
+
+
+    }
+
+    [HttpGet("result")]
+    public IActionResult Result()
+    {
+      return View(model: saveUser);
+    }
+
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
