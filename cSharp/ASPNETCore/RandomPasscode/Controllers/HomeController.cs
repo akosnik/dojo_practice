3c140c128fe114ba76d1369b60e5b3d4188e3670
Allen Kosnik <allen.kosnik@gmail.com>
2022-02-15 13:06:45 -0600

    init commit random passcode

---
+++ cSharp/ASPNETCore/RandomPasscode/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RandomPasscode.Models;
+using Microsoft.AspNetCore.Http;
+
+
+
+namespace RandomPasscode.Controllers
+{
+
+  public class HomeController : Controller
+  {
+    [HttpGet()]
+    public IActionResult Index()
+    {
+      string[] chars = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+      Random rand = new Random();
+      string randomPass = "";
+      for (int i = 0; i < 14; i++)
+      {
+        randomPass = randomPass + chars[rand.Next(chars.Length)];
+      }
+      ViewBag.RandomPass = randomPass;
+
+      if (HttpContext.Session.GetInt32("Count") == null)
+      {
+        HttpContext.Session.SetInt32("Count", 0);
+      }
+      int? count = HttpContext.Session.GetInt32("Count");
+      HttpContext.Session.SetInt32("Count", (int)++count);
+      ViewBag.Count = count;
+
+      return View("Index");
+    }
+  }
+}
