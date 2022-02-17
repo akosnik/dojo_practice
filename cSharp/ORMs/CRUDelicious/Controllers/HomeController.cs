acd58af9d8c8a69513fd7f6824497ee7397c4423
Allen Kosnik <allen.kosnik@gmail.com>
2022-02-17 11:02:17 -0600

    init commit crudelicious

---
+++ cSharp/ORMs/CRUDelicious/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using CRUDelicious.Models;
+
+namespace CRUDelicious.Controllers
+{
+  public class HomeController : Controller
+  {
+    private MyContext _context;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger, MyContext context)
+    {
+      _context = context;
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      List<Dish> AllDishes = _context.Dishes.ToList();
+      return View(model: AllDishes);
+    }
+  }
+}
